e281c13a64db4932c2a80ce834413d5796128902
NRaykov <nediotenev@gmail.com>
2020-04-16 19:06:58 +0300

    Link project to Firebase DB. Create 'saveData' method. Create 'loadData' method

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,11 @@
                 <strong class="navbar-text navbar-right">Your Funds is: {{ funds | currency }}</strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#" @click="endDay">End Day</a></li>
-                    <li class="dropdown">
+                    <li
+                            class="dropdown"
+                            :class="{open: isDropdownOpen}"
+                            @click="isDropdownOpen = !isDropdownOpen"
+                    >
                         <a
                            href="#"
                            class="dropdown-toggle"
@@ -21,8 +25,8 @@
                            aria-haspopup="true"
                            aria-expanded="false">Save & Load</a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
-                            <li><a href="#">Load Data</a></li>
+                            <li><a href="#" @click="saveData">Save Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -34,17 +38,34 @@
 <script>
   import { mapActions } from 'vuex';
   export default {
+    data() {
+      return {
+        isDropdownOpen: false
+      }
+    },
     computed: {
       funds() {
         return this.$store.getters.funds;
       }
     },
     methods: {
-      ...mapActions([
-            'randomizeStocks',
-      ]),
+      ...mapActions({
+        randomizeStocks: 'randomizeStocks',
+        fetchData: 'loadData'
+      }),
       endDay() {
         this.randomizeStocks();
+      },
+      saveData() {
+        const data = {
+          funds: this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPortfolio,
+          stocks: this.$store.getters.stocks
+        };
+        this.$http.put('data.json', data)
+      },
+      loadData() {
+        this.fetchData();
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://vuex-playground.firebaseio.com/";
 
 //Creating Filters (Pipes)
 Vue.filter('currency', (value) => {
